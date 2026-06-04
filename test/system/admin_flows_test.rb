d245069f256da22ffd9e3316117aeb49ae782a12
Hazem Shaban <hazem.shaban.std@gmail.com>
2026-06-04 06:01:32 +0200

    test: add system tests for admin flows and teacher inline actions

---
+++ test/system/admin_flows_test.rb
@@ -0,0 +1,100 @@
+require "application_system_test_case"
+
+class AdminFlowsTest < ApplicationSystemTestCase
+  setup do
+    @admin = create(:user, :admin)
+    @teacher = create(:user, :teacher, full_name: "Dr. System Teacher")
+    # Sign in
+    visit new_session_path
+    fill_in "email_address", with: @admin.email_address
+    fill_in "password", with: "password123"
+    click_button "Sign in"
+  end
+
+  test "admin can perform CRUD on Colleges" do
+    visit admin_colleges_path
+    assert_selector "h1", text: "Colleges"
+
+    # Create
+    click_link "New College"
+    fill_in "Name", with: "Faculty of Computer Science"
+    click_button "Create College"
+    assert_text "College created."
+    assert_text "Faculty of Computer Science"
+
+    # Read/Show
+    college = College.last
+    click_link "Show", href: admin_college_path(college)
+    assert_selector "h1", text: "Faculty of Computer Science"
+
+    # Update
+    visit admin_colleges_path
+    click_link "Edit", href: edit_admin_college_path(college)
+    fill_in "Name", with: "Faculty of Software Engineering"
+    click_button "Update College"
+    assert_text "College updated."
+    assert_text "Faculty of Software Engineering"
+
+    # Delete
+    click_button "Delete"
+    assert_text "College deleted."
+    assert_no_text "Faculty of Software Engineering"
+  end
+
+  test "admin can perform CRUD on Departments" do
+    college = create(:college, name: "Engineering")
+    visit admin_departments_path
+
+    # Create
+    click_link "New Department"
+    fill_in "Name", with: "Aerospace"
+    select "Engineering", from: "College"
+    click_button "Create Department"
+    assert_text "Department created."
+    assert_text "Aerospace"
+
+    # Update
+    department = Department.last
+    visit admin_departments_path
+    click_link "Edit", href: edit_admin_department_path(department)
+    fill_in "Name", with: "Bio-Engineering"
+    click_button "Update Department"
+    assert_text "Department updated."
+    assert_text "Bio-Engineering"
+
+    # Delete
+    click_button "Delete"
+    assert_text "Department deleted."
+    assert_no_text "Bio-Engineering"
+  end
+
+  test "admin can perform CRUD on Subjects" do
+    college = create(:college, name: "Engineering")
+    department = create(:department, name: "Electrical", college: college)
+    visit admin_subjects_path
+
+    # Create
+    click_link "New Subject"
+    fill_in "Code", with: "EE301"
+    fill_in "Name", with: "Signals and Systems"
+    select "Electrical (Engineering)", from: "Department"
+    select "Dr. System Teacher", from: "Teacher"
+    click_button "Create Subject"
+    assert_text "Subject created."
+    assert_text "EE301"
+
+    # Update
+    subject = Subject.last
+    visit admin_subjects_path
+    click_link "Edit", href: edit_admin_subject_path(subject)
+    fill_in "Name", with: "Advanced Signals and Systems"
+    click_button "Update Subject"
+    assert_text "Subject updated."
+    assert_text "Advanced Signals and Systems"
+
+    # Delete
+    click_button "Delete"
+    assert_text "Subject deleted."
+    assert_no_text "Advanced Signals and Systems"
+  end
+end
---
+++ test/system/teacher_inline_actions_test.rb
@@ -0,0 +1,64 @@
+require "application_system_test_case"
+
+class TeacherInlineActionsTest < ApplicationSystemTestCase
+  setup do
+    @teacher = create(:user, :teacher)
+    @subject = create(:subject, teacher: @teacher, name: "Calculus I", code: "MATH101")
+
+    # Sign in
+    visit new_session_path
+    fill_in "email_address", with: @teacher.email_address
+    fill_in "password", with: "password123"
+    click_button "Sign in"
+  end
+
+  test "teacher can view their subject and upload/manage material" do
+    visit subjects_path
+    assert_text "My Subjects"
+    assert_text "Calculus I"
+    assert_text "You teach this"
+
+    click_link "View"
+    assert_selector "h1", text: "Calculus I"
+
+    # Check that upload buttons are present
+    assert_link "Upload Material"
+    assert_link "Manage Files"
+
+    # Click upload and fill form
+    click_link "Upload Material"
+    assert_selector "h1", text: "Upload Material"
+
+    fill_in "Title", with: "Syllabus 2026"
+    attach_file "File", Rails.root.join("test/fixtures/files/test.pdf")
+    click_button "Upload"
+
+    assert_text "Material uploaded."
+    assert_text "Syllabus 2026"
+
+    # We are redirected to Materials Index page where Delete button is present
+    click_button "Delete"
+    assert_text "Material removed."
+    assert_no_text "Syllabus 2026"
+  end
+
+  test "another teacher cannot see upload or manage buttons" do
+    other_teacher = create(:user, :teacher)
+
+    # Sign out
+    click_button "Sign out", match: :first
+
+    # Sign in as other teacher
+    visit new_session_path
+    fill_in "email_address", with: other_teacher.email_address
+    fill_in "password", with: "password123"
+    click_button "Sign in"
+
+    visit subject_path(@subject)
+    assert_selector "h1", text: "Calculus I"
+
+    # Verify upload buttons are not visible for another teacher
+    assert_no_link "Upload Material"
+    assert_no_link "Manage Files"
+  end
+end
