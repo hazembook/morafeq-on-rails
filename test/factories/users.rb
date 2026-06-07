1aa6b9ef588e6f9e96fcf36b287bb494e2219230
Hazem Shaban <hazem.shaban.std@gmail.com>
2026-06-07 05:31:09 +0200

    test: use FactoryBot sequence for user email_address to prevent validation collisions

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     full_name { "Test User" }
-    email_address { "user#{rand(10000)}@example.com" }
+    sequence(:email_address) { |n| "user#{n}@example.com" }
     password { "password123" }
     password_confirmation { "password123" }
     role { :student }
