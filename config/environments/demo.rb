8c522b166e433eca936b39d622ab6cfaa2232ca8
Hazem Shaban <hazem.shaban.std@gmail.com>
2026-06-12 03:26:41 +0200

    fix: remove trailing comma in demo.rb array

--- config/environments/demo.rb
+++ config/environments/demo.rb
@@ -3,6 +3,6 @@ require_relative "production"
 Rails.application.configure do
   config.hosts = [
     "morafeq.hazembook.com",
-    /\A.*\.morafeq\.hazembook\.com\z/,
+    /\A.*\.morafeq\.hazembook\.com\z/
   ]
 end
