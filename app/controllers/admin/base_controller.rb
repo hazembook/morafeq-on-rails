dac6e3bbb11bb3db3a9321e501b3ad91a5bdebc5
Hazem Shaban <hazem.shaban.std@gmail.com>
2026-06-12 03:20:55 +0200

    feat: add restrict_demo before_action to Admin::BaseController
    Refs: morafeq-u66

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,11 +1,16 @@
 module Admin
   class BaseController < ApplicationController
     before_action :require_admin
+    before_action :restrict_demo, if: -> { action_name.in?(%w[create update destroy]) }
 
     private
 
     def require_admin
       redirect_to root_path, alert: t("common.not_authorized") unless Current.user&.admin?
+    end
+
+    def restrict_demo
+      redirect_to request.referer || admin_root_path, alert: t("flash.admin.demo_restricted") if Rails.env.demo?
     end
 
     def log_audit(action, auditable, changes = nil)
